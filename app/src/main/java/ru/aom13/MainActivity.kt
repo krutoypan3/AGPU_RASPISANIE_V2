515d55e6ea9df30bde16786dd6cdcad4ea76cb22
Artem Oganesyan <Krutoypan3@gmail.com>
2024-04-11 19:09:39 +0300

    Попытка - 4

--- app/src/main/java/ru/aom13/MainActivity.kt
+++ app/src/main/java/ru/aom13/MainActivity.kt
@@ -45,10 +45,7 @@ class MainActivity : ComponentActivity() {
 
 class Human(
     /**
-     * Попытка - 3
-     * 906878697895786
-     * aaaaaaaaaaaaaaaaaaaaaaaaaa
-     * НУ блэ я всё ещё нищий
+     * Попытка - 4
      */
     var age: Int,
     var name: String,
