426e28416a9e32ebd775073163517f8ac273530f
Kiku Uchiha <n.u.10.10@mail.ru>
2024-04-13 00:22:52 +0300

    что-то есть, оно зарождается! продолжаю делать эту фигню

--- app/src/main/java/ru/aom13/MainActivity.kt
+++ app/src/main/java/ru/aom13/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.aom13
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,8 +16,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -124,9 +127,33 @@ fun Screen1() {
 fun Screen2() {
 
 }
+@Preview
 @Composable
-private fun BottomNavigation(){
+private fun BottomNavigation(modifier: Modifier = Modifier){
+    NavigationBar(
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
 
+            },
+            label = {
+                Text(text = "Профиль")
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+
+            },
+            label = {
+                Text(text = "Что-то ещё")
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
 }
 
 
