2921d0c1b5885b14e050e6d81600efeb4be130ad
Artem Oganesyan <Krutoypan3@gmail.com>
2024-04-13 19:18:19 +0300

    Добавил карточки 4 гениев

---
+++ app/src/main/java/ru/aom13/model/User.kt
@@ -0,0 +1,11 @@
+package ru.aom13.model
+
+import androidx.annotation.DrawableRes
+
+data class User(
+    @DrawableRes val photoResId: Int,
+    val name: String,
+    val status: String = "",
+    val followersCount: Int,
+    val followingCount: Int,
+)
